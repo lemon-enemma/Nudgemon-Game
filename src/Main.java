94232637d4772063a4ab0dd2e7af497e112a95b2
EmmaLeung <emmal219@nycstudents.net>
2025-11-14 14:47:30 -0500

    11/14

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Nudgemon[] allNudgemon = new Nudgemon[9];
+        allNudgemon[0] = new Nudgemon(1, "Torchicken", "Fire");
+        allNudgemon[1] = new Nudgemon(2, "Mudkipper", "Water");
+        allNudgemon[2] = new Nudgemon(3, "Trekko", "Grass");
+        // starting sequence
+        System.out.print("Hello trainer! My name is Professor Ginkgo. What's yours? \nMy name is: ");
+        String playerName = s.nextLine();
+        System.out.println("Alright, " + playerName + "! Time to pick your starter pokemon. Would you like: \n 1. Torchic? \n 2. Mudkip? \n 3. Treeko?");
 
     }
 }
--- src/Nudgemon.java
+++ src/Nudgemon.java
@@ -1,3 +1,24 @@
 public class Nudgemon {
-    s
+    public String species;
+    public int nudgedexNumber;
+    public String type;
+    public int loveRating;
+
+    public Nudgemon(int n, String s, String t){
+        species = s;
+        nudgedexNumber = n;
+        type = t;
+        loveRating = 0;
+    }
+
+    public void printInfo(){
+        System.out.println("This Nudgemon is a " + species + ". Its type is " + type + ".");
+        if (loveRating == 100){
+            System.out.println(species + "loves you so much! You must take very good care of it.");
+        }
+        else if (loveRating >=90){
+            System.out.println("You have a strong bond with " + species + ". Great job taking care of it!");
+        }
+    }
+
 }
